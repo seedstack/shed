2c7df1a3a4c4830289dd1b51393ac32a6d127610
Adrien LAUER <adrien.lauer@gmail.com>
2016-11-24 17:58:55 +0100

    Add verbose exception

---
+++ src/main/java/org/seedstack/shed/lang/ClassLoaders.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.shed.lang;
+
+public final class ClassLoaders {
+    private ClassLoaders() {
+        // no instantiation allowed
+    }
+
+    /**
+     * Find the most complete class loader by trying the current thread context class loader, then the classloader of the
+     * given class if any, then the class loader that loaded SEED core, then the system class loader.
+     *
+     * @param target the class to get the class loader from if no current thread context class loader is present. May be null.
+     * @return the most complete class loader it found.
+     */
+    public static ClassLoader findMostCompleteClassLoader(Class<?> target) {
+        // Try the most complete class loader we can get
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        // Then fallback to the class loader from a specific class given
+        if (classLoader == null && target != null) {
+            classLoader = target.getClassLoader();
+        }
+
+        // Then fallback to the class loader that loaded this class
+        if (classLoader == null) {
+            classLoader = ClassLoaders.class.getClassLoader();
+        }
+
+        // Then fallback to the system class loader
+        if (classLoader == null) {
+            classLoader = ClassLoader.getSystemClassLoader();
+        }
+
+        // Throw an¬ exception if no classloader was found at all
+        if (classLoader == null) {
+            throw new RuntimeException("Unable to find a classloader");
+        }
+
+        return classLoader;
+    }
+}
---
+++ src/test/java/org/seedstack/shed/lang/VerboseExceptionTest.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.shed.lang;
+
+import org.junit.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VerboseExceptionTest {
+    private enum TotoErrorCode implements VerboseException.ErrorCode {
+        JOKE_MODE, CARAMBAR_MODE;
+    }
+
+    private static class TotoException extends VerboseException {
+        private static final long serialVersionUID = 1L;
+
+        private TotoException(ErrorCode errorCode) {
+            super(errorCode);
+        }
+
+        private TotoException(ErrorCode errorCode, Throwable throwable) {
+            super(errorCode, throwable);
+        }
+    }
+
+    @Test(expected = TotoException.class)
+    public void testCreateNewTotoException1() {
+        throw VerboseException.createNew(TotoException.class, TotoErrorCode.CARAMBAR_MODE)
+                .put("key1", "value1")
+                .put("key2", "value2");
+    }
+
+    @Test(expected = TotoException.class)
+    public void wrap_Should_Work_Fine_WIth_Descendant() {
+        try {
+            throw new NullPointerException();
+        } catch (Exception exception) {
+            throw VerboseException.wrap(TotoException.class, exception, TotoErrorCode.JOKE_MODE)
+                    .put("Error Code", "this is how we do it !");
+        }
+    }
+
+    @Test(expected = VerboseException.class)
+    public void wrap_Should_Work_Fine_WIth_Change_Of_ErrorCode() {
+        try {
+            throw new TotoException(TotoErrorCode.JOKE_MODE);
+        } catch (TotoException e) {
+            throw VerboseException.wrap(TotoException.class, e, TotoErrorCode.CARAMBAR_MODE);
+        }
+    }
+
+    @Test(expected = VerboseException.class)
+    public void wrap_Should_Work_Fine_1() {
+        try {
+            throw new TotoException(TotoErrorCode.JOKE_MODE);
+        } catch (TotoException exception) {
+            throw VerboseException.wrap(TotoException.class, exception, TotoErrorCode.JOKE_MODE)
+                    .put("Error Code", "this is how we do it !");
+        }
+    }
+
+    @Test(expected = VerboseException.class)
+    public void wrap_Should_Work_Fine_2() {
+        try {
+            throw new TotoException(TotoErrorCode.JOKE_MODE);
+        } catch (TotoException exception) {
+            throw VerboseException.wrap(TotoException.class, exception, TotoErrorCode.CARAMBAR_MODE)
+                    .put("Error Code", "this is how we do it !");
+        }
+    }
+
+    @Test
+    public void multiple_causes_should_be_visible() throws Exception {
+        StringWriter stringWriter = new StringWriter();
+        VerboseException.wrap(TotoException.class, VerboseException.wrap(TotoException.class, new RuntimeException("yop"), TotoErrorCode.CARAMBAR_MODE), TotoErrorCode.JOKE_MODE).printStackTrace(new PrintWriter(stringWriter));
+        String text = stringWriter.toString();
+
+        assertThat(text).contains("Caused by: java.lang.RuntimeException: yop");
+        assertThat(text).contains("Caused by: org.seedstack.shed.lang.VerboseExceptionTest$TotoException: (TOTO) Carambar mode");
+        assertThat(text).contains("org.seedstack.shed.lang.VerboseExceptionTest$TotoException: (TOTO) Joke mode");
+    }
+
+    @Test
+    public void infoShouldBeLoaded() throws Exception {
+        VerboseException seedException = VerboseException.createNew(TotoException.class, TotoErrorCode.CARAMBAR_MODE)
+                .put("who", "World")
+                .put("tld", "com");
+        String text = seedException.toString();
+        assertThat(text).contains("Hello World!");
+        assertThat(text).contains("Some fix");
+        assertThat(text).contains("http://some.url.com");
+    }
+}
