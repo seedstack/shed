845e6beb95336638ef066cf4b82d61ef53b97b25
Adrien LAUER <adrien.lauer@gmail.com>
2017-10-06 14:11:51 +0200

    Add some throwing classes

--- src/main/java/org/seedstack/shed/exception/Throwing.java
+++ src/main/java/org/seedstack/shed/exception/Throwing.java
@@ -21,15 +21,80 @@ public final class Throwing {
     @FunctionalInterface
     public interface Consumer<T, X extends Throwable> extends java.util.function.Consumer<T> {
 
-        void apply(T t) throws X;
+        void doAccept(T t) throws X;
 
         @Override
         default void accept(T t) {
             try {
-                apply(t);
+                doAccept(t);
             } catch (Throwable throwable) {
                 Throwing.throwException(throwable);
             }
+            throw new IllegalStateException("Error in throwing consumer");
+        }
+    }
+
+    @FunctionalInterface
+    public interface BiConsumer<T, U, X extends Throwable> extends java.util.function.BiConsumer<T, U> {
+
+        void doAccept(T t, U u) throws X;
+
+        @Override
+        default void accept(T t, U u) {
+            try {
+                doAccept(t, u);
+            } catch (Throwable throwable) {
+                Throwing.throwException(throwable);
+            }
+            throw new IllegalStateException("Error in throwing bi-consumer");
+        }
+    }
+
+    @FunctionalInterface
+    public interface Supplier<T, X extends Throwable> extends java.util.function.Supplier<T> {
+
+        T doGet() throws X;
+
+        @Override
+        default T get() {
+            try {
+                return doGet();
+            } catch (Throwable throwable) {
+                Throwing.throwException(throwable);
+            }
+            throw new IllegalStateException("Error in throwing supplier");
+        }
+    }
+
+    @FunctionalInterface
+    public interface Function<T, R, X extends Throwable> extends java.util.function.Function<T, R> {
+
+        R doApply(T t) throws X;
+
+        @Override
+        default R apply(T t) {
+            try {
+                return doApply(t);
+            } catch (Throwable throwable) {
+                Throwing.throwException(throwable);
+            }
+            throw new IllegalStateException("Error in throwing function");
+        }
+    }
+
+    @FunctionalInterface
+    public interface BiFunction<T, U, R, X extends Throwable> extends java.util.function.BiFunction<T, U, R> {
+
+        R doApply(T t, U u) throws X;
+
+        @Override
+        default R apply(T t, U u) {
+            try {
+                return doApply(t, u);
+            } catch (Throwable throwable) {
+                Throwing.throwException(throwable);
+            }
+            throw new IllegalStateException("Error in throwing function");
         }
     }
 }
